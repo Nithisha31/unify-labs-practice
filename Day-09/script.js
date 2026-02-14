f6bc6020b60281f539990e0d8e18efd7a621280b
Nithisha31 <rasakatlanithisha@gmail.com>
2026-02-14 23:53:40 +0530

    Update script.js

--- Day-09/script.js
+++ Day-09/script.js
@@ -1,17 +1,7 @@
-const themeSwitch = document.getElementById("themeSwitch");
-const form = document.getElementById("contactForm");
-const message = document.getElementById("formMessage");
 
-themeSwitch.addEventListener("change", () => {
-    document.documentElement.setAttribute(
-        "data-theme",
-        themeSwitch.checked ? "dark" : "light"
-    );
-});
-
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    message.textContent = "Form Submitted Successfully!";
-    message.style.color = "green";
-    form.reset();
+document.querySelector('.auth-form').addEventListener('submit',function(e){
+if(!this.checkValidity()){
+e.preventDefault();
+alert('Please fill all fields correctly.');
+}
 });
